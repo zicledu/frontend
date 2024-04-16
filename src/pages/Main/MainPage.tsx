c8ae543b3e0dde1621e9a3b71a6a6c244d453038
6dreamhigh <6dreamhigh@naver.com>
2024-04-17 08:57:17 +0900

    [update] best, new api front

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,49 +1,19 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Container, Flex, SimpleGrid } from "@chakra-ui/react";
+import { Box, Container, Flex, SimpleGrid, Input, Button, IconButton, useColorModeValue, background } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons"
+import axios from "axios";
 import Slider from "../../components/Slider";
 import ClassCard from "../../components/ClassCard";
 import SectionTitle from "../../components/SectionTitle";
 import ContentArea from "../../components/ContentArea";
-
-const TempClassInfo = [
-  {
-    title: "Green Screen, RealTime VFX",
-    desc: "Unreal Engine",
-    classId: 1,
-    imgSrc: "/images/1.png",
-  },
-  {
-    title: "Filming in Green Screen",
-    desc: "VIVEMARS",
-    classId: 2,
-    imgSrc: "/images/2.png",
-  },
-  {
-    title: "Cosmic Horror",
-    desc: "Unreal Engine",
-    classId: 3,
-    imgSrc: "/images/3.png",
-  },
-  {
-    title: "Led wall Installation",
-    desc: "Unreal Engine",
-    classId: 4,
-    imgSrc: "/images/4.png",
-  },
-  {
-    title: "Led Wall Virtual Production",
-    desc: "Unreal Engine",
-    classId: 5,
-    imgSrc: "/images/5.png",
-  },
-  {
-    title: "ANAMORPHIC Garden",
-    desc: "Unreal Engine",
-    classId: 6,
-    imgSrc: "/images/6.png",
-  },
-];
+// 강의 정보에 대한 타입 정의
+type CourseInfo = {
+  title: string;
+  description: string;
+  classId: number;
+  thumbnailPath: string;
+};
 
 const ClassCardList = ({ children }: { children: ReactNode }) => (
   <SimpleGrid
@@ -58,6 +28,54 @@ const ClassCardList = ({ children }: { children: ReactNode }) => (
 
 function MainPage() {
   const navigate = useNavigate();
+  // 최고의 결과를 저장할 상태
+  const [bestResults, setBestResults] = useState<CourseInfo[]>([]);
+  // 최신의 결과를 저장할 상태
+  const [newResults, setNewResults] = useState<CourseInfo[]>([]);
+  // 검색 결과를 저장할 상태
+  const [keyword, setKeyword] = useState("");
+  const [searchResults, setSearchResults] = useState([])
+
+  // 페이지 로드 시 최고의 결과와 최신의 결과를 가져오는 useEffect
+  useEffect(() => {
+    handleBest();
+    handleNew();
+  }, []); // 페이지가 로드될 때 한 번만 실행
+// 서버에서 최고의 결과를 가져오는 함수
+const handleBest = () => {
+  axios.get('http://localhost:8080/course/best')
+  .then((response) => {
+    setBestResults(response.data.data); // 최고의 결과를 state에 저장
+  })
+  .catch((error) => {
+    console.error('Error fetching best results', error);
+  });
+};
+
+// 서버에서 최신의 결과를 가져오는 함수
+const handleNew = () => {
+  axios.get('http://localhost:8080/course/new')
+  .then((response) => {
+    setNewResults(response.data.data); // 최신의 결과를 state에 저장
+  })
+  .catch((error) => {
+    console.error('Error fetching new results', error);
+  });
+};
+
+  const handleSearch = () => {
+    axios.get('http://localhost:8080/course/search', {
+      params: {
+        keyword: keyword
+      }
+    })
+    .then((response) => {
+      setSearchResults(response.data);
+    })
+    .catch((error) => {
+      console.error('Error fetching search results', error);
+    });
+  };
 
   return (
     <>
@@ -70,38 +88,61 @@ function MainPage() {
           width={"100%"}
           gap={4}
         >
+          <Box mx="auto" width="50%">
+            <Flex align="center" justify="center">
+              <input
+                type="text"
+                placeholder="검색어를 입력하세요"
+                value={keyword}
+                onChange={(event) => setKeyword(event.target.value)}
+                style={{ padding: "8px", fontSize: "16px", borderRadius: "999px", width: "100%", paddingLeft: "20px" }}
+                />
+                <Button 
+                  onClick={handleSearch}
+                  borderRadius="full"
+                  bgColor={useColorModeValue("gray.200", "gray.700")}
+                  _hover={{ bgColor: useColorModeValue("gray.300", "gray.600")}}
+                  ml={2}
+                >
+                  <SearchIcon />
+                 </Button>
+              </Flex>
+          </Box>
           <Box>
             <SectionTitle title={"BEST"} />
             <ClassCardList>
-              {TempClassInfo.map((item, idx) => (
+              {bestResults.map((item, idx) => (
                 <ClassCard
                   key={idx}
                   title={item.title}
-                  desc={item.desc}
+                  desc={item.description}
                   onClick={() => navigate(`/class/${item.classId}`)}
-                  imgSrc={item.imgSrc}
+                  imgSrc={item.thumbnailPath}
                 />
               ))}
             </ClassCardList>
           </Box>
+          
           <Box>
-            <SectionTitle title={"신규 오픈 클래스"} />
+            <SectionTitle title={"New"} />
             <ClassCardList>
-              {TempClassInfo.map((item, idx) => (
+              {newResults.map((item, idx) => (
                 <ClassCard
                   key={idx}
                   title={item.title}
-                  desc={item.desc}
+                  desc={item.description}
                   onClick={() => navigate(`/class/${item.classId}`)}
-                  imgSrc={item.imgSrc}
+                  imgSrc={item.thumbnailPath}
                 />
               ))}
             </ClassCardList>
           </Box>
+          
         </Flex>
       </ContentArea>
     </>
   );
 }
+
 
 export default MainPage;
