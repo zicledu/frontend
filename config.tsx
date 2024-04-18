3c3048016d017ae5dddd9bbf21b0fa45d414b1fb
shoon95 <tkdgns5817@gmail.com>
2024-04-18 18:39:51 +0900

    [FIX] config 파일 임의 수정

--- config.tsx
+++ config.tsx
@@ -1,4 +1,5 @@
-const IP_ADDRESS = import.meta.env.VITE_IP_ADDRESS || import.meta.env.GIT_IP_ADDRESS;
+// const IP_ADDRESS = import.meta.env.VITE_IP_ADDRESS || import.meta.env.GIT_IP_ADDRESS;
+const IP_ADDRESS = "https://zhl2juvik8.execute-api.ap-northeast-2.amazonaws.com/lms";
 
 export const API = {
     COURSE_LIST_BY_BEST : `${IP_ADDRESS}/course/best`,
@@ -6,7 +7,7 @@ export const API = {
     COURSE_LIST_BY_USERID : `${IP_ADDRESS}/course/userId`,
     COURSE_LIST_BY_SEARCH : `${IP_ADDRESS}/course`,
     LOGOUT : `${IP_ADDRESS}/logout`,
-    LOGIN : `${IP_ADDRESS}/lgoin`,
+    LOGIN : `${IP_ADDRESS}/login`,
     SIGNUP : `${IP_ADDRESS}/join`,
     REFRESH : `${IP_ADDRESS}/refresh`
     
